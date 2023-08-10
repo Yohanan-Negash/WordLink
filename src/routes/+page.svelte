a21b322c4aedf4843e9fbbc3b83e6d7d40e04532
Yohanan-Negash <yoniget066@gmail.com>
2023-08-10 17:44:19 -0400

    Setup TailwindCSS

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<header>
+  <h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+  <p class="text-l mt-10 ">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>  
+</header>
+
+
